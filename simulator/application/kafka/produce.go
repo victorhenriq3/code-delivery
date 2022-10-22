9636b36e63ca68a7e149f3dd2af3003a2a3125a8
victorhenriqc <vic.1601@outlook.com>
2022-10-22 16:43:59 -0300

    finished a consumer with kafka

---
+++ simulator/application/kafka/produce.go
@@ -0,0 +1,28 @@
+package kafka
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	route2 "github.com/victorhenriq3/code-delivery/simulator/application/route"
+	"github.com/victorhenriq3/code-delivery/simulator/infra/kafka"
+)
+
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := route2.NewRoute()
+	json.Unmarshal(msg.Value, &route)
+	route.LoadPositions()
+	positions, err := route.ExportJsonPositions()
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	for _, p := range positions {
+		kafka.Publish(p, os.Getenv("KafkaProduceTopic"), producer)
+		time.Sleep(time.Microsecond * 500)
+	}
+}
--- simulator/main.go
+++ simulator/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/joho/godotenv"
-	route2 "github.com/victorhenriq3/code-delivery/simulator/application/route"
+	kafka2 "github.com/victorhenriq3/code-delivery/simulator/application/kafka"
+	"github.com/victorhenriq3/code-delivery/simulator/infra/kafka"
 )
 
 func init() {
@@ -16,12 +18,12 @@ func init() {
 }
 
 func main() {
-	route := route2.Route{
-		ID:       "1",
-		ClientID: "1",
-	}
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
 
-	route.LoadPositions()
-	stringjson, _ := route.ExportJsonPositions()
-	fmt.Println(stringjson[0])
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
+	}
 }
